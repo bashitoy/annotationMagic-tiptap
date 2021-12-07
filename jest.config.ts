4284db145ccd4af0350ae6dd450733f203a331af
bashitoy <140951892+bashitoy@users.noreply.github.com>
2021-12-07 23:00:29 +0000

    Revert "crypto: talitos - convert to use be16_add_cpu()"

--- jest.config.ts
+++ jest.config.ts
@@ -38,4 +38,4 @@ const config: Config = {
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
 
-  // A li
+  // A list of reporter names that Jest uses when writing covera
