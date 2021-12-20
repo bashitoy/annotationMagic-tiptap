6dfa66fe94f23dd618544321915de26420ee8fd2
bashitoy <140951892+bashitoy@users.noreply.github.com>
2021-12-20 10:17:41 +0000

    adds todo

--- jest.config.ts
+++ jest.config.ts
@@ -38,4 +38,7 @@ const config: Config = {
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
 
-  // A list of reporter names that Jest uses when writing covera
+  // A list of reporter names that Jest uses when writing coverage reports
+  // coverageReporters: [
+  //   "json",
+ 
