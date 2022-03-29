bdcb49d3deac613ca7fbcc28096d95b6f3196413
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-03-29 10:36:21 +0000

    Minor optimisations

--- jest.config.ts
+++ jest.config.ts
@@ -128,4 +128,5 @@ const config: Config = {
   // The root directory that Jest should scan for tests and modules within
   // rootDir: undefined,
 
-  // A list of paths to directori
+  // A list of paths to directories that Jest should use to search for files in
+  // 
