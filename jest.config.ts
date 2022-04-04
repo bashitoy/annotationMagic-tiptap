0df8305d15058e97b3901f821f84330f5ca2f6a4
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-04-04 12:16:48 +0000

    luna-next-cardshell: Bump SRCREV

--- jest.config.ts
+++ jest.config.ts
@@ -138,3 +138,5 @@ const config: Config = {
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
   // setupFiles: [],
+
+  // A list of paths to modules that run some code to c
