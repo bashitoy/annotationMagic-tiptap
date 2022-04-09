6ab26935c590abf9bd1ce9c6a13e89a2a892e9cf
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-04-09 18:06:50 +0000

    Edit image

--- jest.config.ts
+++ jest.config.ts
@@ -142,4 +142,7 @@ const config: Config = {
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
 
-  // The number of seconds after which a test is considered as slow and reported a
+  // The number of seconds after which a test is considered as slow and reported as such in the results.
+  // slowTestThreshold: 5,
+
+  // A
