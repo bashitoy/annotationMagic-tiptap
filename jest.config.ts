4ba1190dc51d14e3cd09f3dd74d65f85e11bdcb0
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-04-16 17:18:09 +0000

    better fix

--- jest.config.ts
+++ jest.config.ts
@@ -166,4 +166,6 @@ const config: Config = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
   //   "\\\\node_modules\\\\"
-  /
+  // ],
+
+  // The regexp pattern or array of patter
