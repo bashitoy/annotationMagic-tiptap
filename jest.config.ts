6ea4941a31cdcd7045046d7958679943bf618e73
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-04-20 18:05:20 +0000

    update po

--- jest.config.ts
+++ jest.config.ts
@@ -182,4 +182,5 @@ const config: Config = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
-  //
+  //   "\\\\node_modules\\\\",
+  //   "\\.pnp\\.[^\\\\]+$
