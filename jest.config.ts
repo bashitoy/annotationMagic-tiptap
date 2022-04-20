890aefbccc7200eafdbea1a2c6acb3fb2f089558
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-04-20 13:08:46 +0000

    py38

--- jest.config.ts
+++ jest.config.ts
@@ -180,4 +180,6 @@ const config: Config = {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
 
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will ski
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  // transformIgnorePatterns: [
+  //
