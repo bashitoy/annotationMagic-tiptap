77bb7b77103d235d67cda76bc635338d485e0500
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-04-28 09:29:30 +0000

    First Checkin

--- jest.config.ts
+++ jest.config.ts
@@ -187,3 +187,6 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
+  // unmockedModulePathPatterns: undefined,
+
+  // Indicates 
