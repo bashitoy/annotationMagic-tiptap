01586ad9a7b6bf1ef7f0290de2f95dc8c50908f9
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-04-29 08:15:18 +0000

    Update protobufs' Go code.

--- jest.config.ts
+++ jest.config.ts
@@ -195,4 +195,8 @@ const config: Config = {
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
 
-  // Whether to use watchman for file craw
+  // Whether to use watchman for file crawling
+  // watchman: true,
+};
+
+export default config;
