8f0275ff0092f92a695322fdd59dccc01ef8f4c1
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-07-31 12:24:09 +0000

    login test

--- src/extension/annotation-magic.ts
+++ src/extension/annotation-magic.ts
@@ -2,4 +2,6 @@ import { Extension } from "@tiptap/core";
 import { AnnotationPlugin, AnnotationPluginKey } from "./pm/annotation-plugin";
 import { Annotation } from "../contracts";
 
-export interface R
+export interface RenderStyles {
+  rightFragment: string;
+  leftFragment:
