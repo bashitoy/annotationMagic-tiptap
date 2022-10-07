bfc615a883b2f2d5900f643b513d1db12a12d389
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-10-07 19:05:49 +0000

    Removed some TODOs

--- src/extension/pm/annotation-decoration.ts
+++ src/extension/pm/annotation-decoration.ts
@@ -43,4 +43,6 @@ export class AnnotationDecoration<K> implements Annotation<K> {
     return JSON.stringify({
       id: this.id,
       data: this.data,
-      fro
+      from: this.from,
+      to: this.to,
+      HTMLAttributes: 
