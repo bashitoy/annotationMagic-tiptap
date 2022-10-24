6a1d806439e5c85d38064cc231c1d5ed07b5dc62
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-10-24 23:02:52 +0000

    add theme

--- src/extension/pm/annotation-state.ts
+++ src/extension/pm/annotation-state.ts
@@ -4,4 +4,7 @@ import { Decoration, DecorationSet } from "@tiptap/pm/view";
 import { AnnotationDecoration } from "./annotation-decoration";
 import { AnnotationPluginKey } from "./annotation-plugin";
 import {
-  AddAnnotationActio
+  AddAnnotationAction,
+  DeleteAnnotationAction,
+  RenderStyles,
+  
