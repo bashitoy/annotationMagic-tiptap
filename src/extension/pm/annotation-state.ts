6a5392ec1acb4962c56510f52212fbdd10c3e215
bashitoy <140951892+bashitoy@users.noreply.github.com>
2022-10-24 17:45:43 +0000

    Fixed ordering

--- src/extension/pm/annotation-state.ts
+++ src/extension/pm/annotation-state.ts
@@ -2,4 +2,6 @@ import { EditorState, Transaction } from "@tiptap/pm/state";
 import { Decoration, DecorationSet } from "@tiptap/pm/view";
 
 import { AnnotationDecoration } from "./annotation-decoration";
-import { AnnotationPluginKey } from
+import { AnnotationPluginKey } from "./annotation-plugin";
+import {
+  AddAnnotationActio
