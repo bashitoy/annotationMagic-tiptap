f3fc51307e70ab8c24dbfaf4c23f0ec80e4d4819
bashitoy <140951892+bashitoy@users.noreply.github.com>
2023-04-03 20:39:34 +0000

    iommu/amd: Initialize device table after dma_ops

--- src/extension/rendering/engine.spec.ts
+++ src/extension/rendering/engine.spec.ts
@@ -8,3 +8,40 @@ import { Annotation, AnnotationFragment } from "../../contracts/annotation";
 
 const conflictTestCases = [
   { a: { from: 1, to: 2 }, b: { from: 2, to: 3 }, overlapping: true },
+  { b: { from: 1, to: 2 }, a: { from: 2, to: 3 }, overlapping: true },
+  { a: { from: 1, to: 3 }, b: { from: 2, to: 3 }, overlapping: true },
+  { b: { from: 1, to: 3 }, a: { from: 2, to: 3 }, overlapping: true },
+  { a: { from: 1, to: 2 }, b: { from: 3, to: 4 }, overlapping: false },
+  { b: { from: 1, to: 2 }, a: { from: 3, to: 4 }, overlapping: false },
+];
+
+describe("Test conflicting annotation", () => {
+  test("Should declare non conflicting test cases as such", () => {
+    for (let testCase of conflictTestCases) {
+      expect(
+        isConflicting(
+          testCase.a.from,
+          testCase.a.to,
+          testCase.b.from,
+          testCase.b.to,
+        ),
+      ).toBe(testCase.overlapping);
+    }
+  });
+});
+
+describe("Test sorting of annotation list", () => {
+  const testList: Annotation<any>[] = [
+    {
+      id: "abc",
+      from: 5,
+      to: 6,
+      displayName: "X",
+      tag: "X",
+    },
+    {
+      id: "abc",
+      from: 3,
+      to: 9,
+      displayName: "X",
+      tag: "X",
