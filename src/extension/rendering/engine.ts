4ac91120ecb0912478fac73041f73a79117f0570
bashitoy <140951892+bashitoy@users.noreply.github.com>
2023-05-10 15:19:44 +0000

    alignment

--- src/extension/rendering/engine.ts
+++ src/extension/rendering/engine.ts
@@ -18,4 +18,5 @@ export const isConflicting = (
 ): boolean => {
   // case 1: (non-conflicting) A is before B
   if (fromA < toB && toA < fromB) return false;
-  // case 2: (non-conflicting
+  // case 2: (non-conflicting) B is before A
+  if (fromB < toA && toB < fromA) return f
