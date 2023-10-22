bda7fb091ed27f95fea2ad1efbeabdbc10cb87ab
bashitoy <140951892+bashitoy@users.noreply.github.com>
2023-10-22 15:48:12 +0000

    Add missing dep

--- src/extension/rendering/engine.ts
+++ src/extension/rendering/engine.ts
@@ -76,4 +76,4 @@ export const createAnnotationRendering = (
             ]
               ? "fragment-right"
               : "normal";
-            le
+            let from: number = annotationFragmentation[action.annotatio
