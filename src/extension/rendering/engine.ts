b428b56ee15d32ccbd5966b4fdcf2c1a921a3725
bashitoy <140951892+bashitoy@users.noreply.github.com>
2023-11-14 18:51:26 +0000

    Czech translation

--- src/extension/rendering/engine.ts
+++ src/extension/rendering/engine.ts
@@ -89,4 +89,6 @@ export const createAnnotationRendering = (
             }
           } else if (
             actionStackPeek.action === "open" &&
-            action.action === "c
+            action.action === "close"
+          ) {
+            // annotation is closed 
