479bbd89b0e8c0e26b99002f098d41a1f55a2ace
bashitoy <140951892+bashitoy@users.noreply.github.com>
2023-11-28 20:03:54 +0000

    Fix bootstrap

--- src/extension/rendering/engine.ts
+++ src/extension/rendering/engine.ts
@@ -96,4 +96,4 @@ export const createAnnotationRendering = (
             let indexOfActionToRemove = openAnnotationStack.findIndex((a) => {
               return (
                 a.textAnchor === annotations[action.annotationIndex].from &&
-           
+                a.annotationIndex === action.annotat
